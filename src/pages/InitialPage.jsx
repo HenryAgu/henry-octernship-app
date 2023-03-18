6ff07ebdd48dd6c6739d90bb402fe6b0656ca78e
Henry <aguhenrychuks@gmail.com>
2023-03-18 14:50:18 +0100

    feat: landing page UI updated

--- src/pages/InitialPage.jsx
+++ src/pages/InitialPage.jsx
@@ -5,7 +5,12 @@ import '../css/InitialPage.css'
 
 const InitialPage = () => {
   return (
-    <div className='initial_page'>initialPage</div>
+    <div className='initial_page'>
+      <div className="form">
+        <input type="text" placeholder='Enter a text' required/>
+        <button>Submit</button>
+      </div>
+    </div>
   )
 }
 
