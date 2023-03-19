6fa5f1cd5ec77c87baac29ab724f1b7c8c4cb453
Henry <aguhenrychuks@gmail.com>
2023-03-19 20:14:09 +0100

    feat: colours for each alphabet of array updated

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React,{useState} from "react";
 
 // react icons
 import { MdKeyboardBackspace } from "react-icons/md";
+import { ImBin } from "react-icons/im";
+
+// classname
+import classNames from "classnames";
 
 // Link
 import { Link } from "react-router-dom";
@@ -19,7 +23,35 @@ const result = ({ input }) => {
         </div>
       </Link>
       {result.map((item) => (
-        <div className="result_card" >
+        <div className={classNames({
+          'letter-a': item === 'a',
+          'letter-b': item === 'b',
+          'letter-c': item === 'c',
+          'letter-d': item === 'd',
+          'letter-e': item === 'e',
+          'letter-f': item === 'f',
+          'letter-g': item === 'g',
+          'letter-h': item === 'h',
+          'letter-i': item === 'i',
+          'letter-j': item === 'j',
+          'letter-k': item === 'k',
+          'letter-l': item === 'l',
+          'letter-m': item === 'm',
+          'letter-n': item === 'n',
+          'letter-o': item === 'o',
+          'letter-p': item === 'p',
+          'letter-q': item === 'q',
+          'letter-r': item === 'r',
+          'letter-s': item === 's',
+          'letter-t': item === 't',
+          'letter-u': item === 'u',
+          'letter-v': item === 'v',
+          'letter-w': item === 'w',
+          'letter-x': item === 'x',
+          'letter-y': item === 'y',
+          'letter-z': item === 'z',
+        })}>
+          <div className="delete"><ImBin/></div>
           <p>{item}</p>
         </div>
       ))}
